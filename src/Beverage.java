e92aee7854d8dd7cd915808ecb9db0023f61a357
Claudia Trujillo <claudia.trujillo@astrazeneca.com>
2019-02-20 11:06:19 -0600

    This class contains the principal attributes to be heritaged to others classes

--- src/Beverage.java
+++ src/Beverage.java
@@ -3,6 +3,18 @@ public class Beverage {
     private Double priceBeverage;
     private Boolean addCream;
 
+
+    public String getNameBeverage() { return nameBeverage; }
+    public void setNameBeverage(String nameBeverage) { this.nameBeverage = nameBeverage; }
+
+    public Double getPriceBeverage() { return priceBeverage; }
+    public void setPriceBeverage(Double priceBeverage) { this.priceBeverage = priceBeverage; }
+
+    public Boolean getAddCream() { return addCream; }
+    public void setAddCream(Boolean addCream) { this.addCream = addCream; }
+
+
+    //ENUM TO SIZES, BECAUSE ALWAYS ARE GOING TO BE THE SAME FOR BEVERAGE.
     enum Size {
         SMALL, MEDIUM, LARGE;
     }
@@ -10,7 +22,8 @@ public class Beverage {
 
     @Override
     public String toString() {
+        System.out.println("");
+
         return super.toString();
     }
-
 }
