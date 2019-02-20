f7d2caed7cea5ed0a0105e14edbe232d0b60bd1a
Claudia Trujillo <claudia.trujillo@astrazeneca.com>
2019-02-20 09:55:32 -0600

    This class is going to be the base for other clases, and contains some attributes name, price, cream, and enum forbeverages´ sizes.

---
+++ src/Beverage.java
@@ -0,0 +1,16 @@
+public class Beverage {
+    private String nameBeverage;
+    private Double priceBeverage;
+    private Boolean addCream;
+
+    enum Size {
+        SMALL, MEDIUM, LARGE;
+    }
+
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+}
