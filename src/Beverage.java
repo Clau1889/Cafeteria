304ddd41a72e109d681e91dd605ef392c8321501
Claudia Trujillo <claudia.trujillo@astrazeneca.com>
2019-02-21 12:19:04 -0600

    Create conditions to accept values within setsMethods.

--- src/Beverage.java
+++ src/Beverage.java
@@ -1,18 +1,9 @@
-public class Beverage {
+import java.lang.reflect.Array;
+import java.util.List;
+
+public class Beverage extends Order{
     private String nameBeverage;
     private Double priceBeverage;
-    private Boolean addCream;
-
-
-    public String getNameBeverage() { return nameBeverage; }
-    public void setNameBeverage(String nameBeverage) { this.nameBeverage = nameBeverage; }
-
-    public Double getPriceBeverage() { return priceBeverage; }
-    public void setPriceBeverage(Double priceBeverage) { this.priceBeverage = priceBeverage; }
-
-    public Boolean getAddCream() { return addCream; }
-    public void setAddCream(Boolean addCream) { this.addCream = addCream; }
-
 
     //ENUM TO SIZES, BECAUSE ALWAYS ARE GOING TO BE THE SAME FOR BEVERAGE.
     enum Size {
@@ -20,10 +11,45 @@ public class Beverage {
     }
 
 
-    @Override
-    public String toString() {
-        System.out.println("");
+    public String getNameBeverage() { return nameBeverage; }
+    public void setNameBeverage(String nameBeverage) { this.nameBeverage = nameBeverage;
 
-        return super.toString();
+        switch (nameBeverage.toLowerCase()){
+            case "coffee":
+                this.nameBeverage= nameBeverage;
+                break;
+
+            case "tea":
+                this.nameBeverage= nameBeverage;
+                break;
+
+            case "frapuccino":
+                this.nameBeverage=  nameBeverage;
+                break;
+
+            default:
+                System.out.println("Sorry, this beverage is unavailable.");
+        }
     }
+
+    public Double getPriceBeverage() { return priceBeverage; }
+    public void setPriceBeverage(Double priceBeverage) { this.priceBeverage = priceBeverage;
+           for(Size sizes: Size.values()){
+
+               if(sizes.equals(Size.SMALL)){
+                   setPriceBeverage(35.00);
+               }else if (sizes.equals(Size.MEDIUM)){
+                   setPriceBeverage(45.00);
+               }else if (sizes.equals(Size.LARGE)){
+                   setPriceBeverage(55.00);
+               }else {
+                   System.out.println("This size is unavailable.");
+               }
+           }
+    }
+
+
+
+
+
 }
